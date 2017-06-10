e40f9100a4bfb798692e08c228105e0572c5c6ea
Emilia Pezda <emilia.ciejka@gmail.com>
2017-06-10 11:20:32 +0100

    update README

--- public/admin/addUser.php
+++ public/admin/addUser.php
@@ -11,6 +11,5 @@ $addComment->setUserId(2);
 $addComment->setPostId(5);
 $addComment->setText('this is second comment');
 $addComment->setCreationDate(date('Y-m-d H:i:s'));
-print_r($addComment);
 
 $addComment->saveToDB($connection);
